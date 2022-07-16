a26b1b3072fd8ef5633d57fb018606f4cba19b4d
Talia Blain <talia.blain23@gmail.com>
2022-07-16 11:11:43 +0100

    working on a calculator application in Java

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,29 +4,23 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { performCalculations();}
 
+    static void performCalculations(){
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d,92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d,3.0d);
 
         for(MathEquation equation : equations) {
             equation.execute();
             System.out.println("result = " + equation.result);
         }
 
+        System.out.println("Average Result = " + MathEquation.getAverageResult());
 
-
-        }
-
-    private static MathEquation create(double leftVal, double rightVal, char opCode){
-        MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
-        return equation;
     }
+
 }
--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -6,6 +6,22 @@ public class MathEquation {
     char opCode;
     double result;
 
+    private static int numberofCalculations;
+    private static double sumOfResults;
+
+    public MathEquation() {}
+
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal)
+    {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
     void execute() {
         switch(opCode) {
             case 'a':
@@ -25,5 +41,20 @@ public class MathEquation {
                 result = 0.0d;
                 break;
         }
+
+        numberofCalculations++;
+        sumOfResults += result;
     }
+
+    public static double getAverageResult() {
+        return sumOfResults / numberofCalculations;
+    }
+
+
+    public double getLeftVal() { return leftVal;}
+    public void setLeftVal(double leftVal) { this.leftVal = leftVal; }
+    public double getRightVal() { return rightVal; }
+    public void setRightVal(double rightVal) { this.rightVal = rightVal;}
+
+
 }
